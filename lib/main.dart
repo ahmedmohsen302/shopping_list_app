6bfc59016f262a5493c3aea9fc5526aa2447911b
ahmedmohsen302 <ahmedthemohsen@gmail.com>
2025-05-01 02:46:23 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ShoppingListApp());
+}
+
+class ShoppingListApp extends StatelessWidget {
+  const ShoppingListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
