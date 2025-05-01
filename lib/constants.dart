afdfc8baae2f1dd151d5a425bda733f76b3f4339
ahmedmohsen302 <ahmedthemohsen@gmail.com>
2025-05-01 03:11:44 +0300

    main theme added

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+final kColorScheme = ColorScheme.fromSeed(seedColor: const Color(0x9B5D036D));
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/constants.dart';
+import 'package:shopping_list_app/views/home_view.dart';
 
 void main() {
   runApp(const ShoppingListApp());
@@ -9,6 +11,20 @@ class ShoppingListApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          color: kColorScheme.primary,
+          centerTitle: true,
+          foregroundColor: kColorScheme.onPrimary,
+          titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: kColorScheme.shadow,
+      ),
+      home: HomeView(),
+    );
   }
 }
